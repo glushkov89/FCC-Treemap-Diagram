9e22a05efb8a71b377ca612861663750ca4edb56
glushkov89 <kellerman117@gmail.com>
2018-09-05 15:09:39 +0300

    All tests passed.

--- script/app.js
+++ script/app.js
@@ -33,19 +33,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const URL =
 		"https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json";
+	//"https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json";
+	//"https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json";
 
 	const d3 = require("d3");
 
 	/*------------Size---------------*/
 	//Margins
-	const mg = { t: 70, b: 90, r: 30, l: 70 };
+	const mg = { t: 70, b: 90, r: 30, l: 30 };
 	//Diag
-	const dia = [1500, 650];
+	const dia = [1650, 650];
 	//SVG
 	const s = {
 		h: mg.t + dia[1] + mg.b,
 		w: mg.l + dia[0] + mg.r
 	};
+	//Legend
+	const lgnd = { rec: 20, mgt: 5, mgl: 5, w: 150 };
+	//Color
+	const color = d3.schemePaired.concat(d3.schemeDark2);
 
 	/*-------------SVG--------------*/
 	let svg = d3
@@ -56,11 +62,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	let sel = svg
 		.append("g")
-		.attr("x", mg.l)
-		.attr("y", mg.t)
+		.attr("transform", (d) => `translate(${mg.l},${mg.t})`)
 		.attr("height", dia[0])
 		.attr("width", dia[1]);
-	//.attr("class", "diagram");
+
+	let legend = svg
+		.append("g")
+		.attr("id", "legend")
+		.attr("height", lgnd.mgt * 2 + lgnd.rec)
+		.attr("transform", (d) => `translate(${mg.l},${mg.t + dia[1]})`);
 
 	/*----------Tooltip------------*/
 	let tooltipDiv = d3
@@ -72,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		.attr("class", "mytooltip");
 
 	/*--------Functions to process data and draw diagrams---------*/
-	const generateColorScale = (arr) => {
+	const generateCategoryList = (arr) => {
 		let dmn = arr.reduce((acc, obj) => {
 			if (obj.depth !== 0 && obj.height > 0 && !acc.includes(obj.data.name)) {
 				acc.push(obj.data.name);
@@ -90,26 +100,126 @@ document.addEventListener("DOMContentLoaded", function() {
 		return treeLayout(root);
 	};
 
-	const drawDiagram = (arr, sl, tlt, color) => {
-		//console.log(arr);
-		sl.selectAll("rect")
+	//From http://bl.ocks.org/mundhradevang/1387786 (adjusted)
+	function fontSize(d) {
+		if (d.height === 0) {
+			var size = (d.x1 - d.x0) / 5;
+			var words = d.data.name.split(" ");
+			var word = words[0];
+			var width = d.x1 - d.x0;
+			var height = d.y1 - d.y0;
+			d3.select(this)
+				.style("font-size", size + "px")
+				.text(word);
+			while (
+				(this.getBBox().width >= width || this.getBBox().height >= height) &&
+				size > 12
+			) {
+				size--;
+				d3.select(this).style("font-size", size + "px");
+				this.firstChild.data = word;
+			}
+		}
+	}
+
+	//From http://bl.ocks.org/mundhradevang/1387786 (adjusted)
+	function wordWrap(d) {
+		if (d.height === 0) {
+			var words = d.data.name.split(" ");
+			//	console.log(words);
+			var line = new Array();
+			var length = 0;
+			var text = "";
+			var width = d.x1 - d.x0;
+			var height = d.y1 - d.y0;
+			var word;
+			do {
+				word = words.shift();
+				line.push(word);
+				if (words.length)
+					this.firstChild.data = line.join(" ") + " " + words[0];
+				else this.firstChild.data = line.join(" ");
+				length = this.getBBox().width;
+				if (length < width && words.length) {
+				} else {
+					text = line.join(" ");
+					this.firstChild.data = text;
+					if (this.getBBox().width > width) {
+						text = d3
+							.select(this)
+							.select(function() {
+								return this.lastChild;
+							})
+							.text();
+						text = text + "...";
+						d3.select(this)
+							.select(function() {
+								return this.lastChild;
+							})
+							.text(text);
+						d3.select(this).classed("wordwrapped", true);
+						break;
+					} else;
+
+					if (text != "") {
+						d3.select(this)
+							.append("svg:tspan")
+							.attr("x", 0)
+							.attr("dx", "0.15em")
+							.attr("dy", "0.9em")
+							.text(text);
+					} else;
+
+					if (this.getBBox().height > height && words.length) {
+						text = d3
+							.select(this)
+							.select(function() {
+								return this.lastChild;
+							})
+							.text();
+						text = text + "...";
+						d3.select(this)
+							.select(function() {
+								return this.lastChild;
+							})
+							.text(text);
+						d3.select(this).classed("wordwrapped", true);
+
+						break;
+					} else;
+
+					line = new Array();
+				}
+			} while (words.length);
+			this.firstChild.data = "";
+		}
+	}
+
+	const drawDiagram = (arr, categ, sl, tlt, clr) => {
+		let groups = sl
+			.selectAll("rect")
 			.data(arr)
 			.enter()
+			.append("g")
+			.attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+
+		groups
 			.append("rect")
-			.attr("x", (d) => d.x0)
-			.attr("y", (d) => d.y0)
 			.attr("width", (d) => d.x1 - d.x0)
 			.attr("height", (d) => d.y1 - d.y0)
 			.attr("class", "tile")
+			.style("stroke", "white")
+			.attr(
+				"fill",
+				(d) =>
+					clr[categ.indexOf(d.height === 0 ? d.data.category : d.data.name)]
+			)
 			.attr("data-name", (d) => d.data.name)
 			.attr(
 				"data-category",
 				(d) => (d.height === 0 ? d.data.category : d.data.name)
 			)
 			.attr("data-value", (d) => d.value)
-			.attr("fill", (d) =>
-				color(d.height === 0 ? d.data.category : d.data.name)
-			)
 			.on("mouseover", (d) => {
 				tlt
 					.html(
@@ -131,194 +241,112 @@ document.addEventListener("DOMContentLoaded", function() {
 					.duration(300)
 					.style("opacity", 0);
 			});
+
+		groups
+			.append("text")
+			.attr("dx", 4)
+			.attr("dy", 15)
+			.each(fontSize)
+			.each(wordWrap);
+	};
+
+	const generateTitleDescription = (sv, d) => {
+		let ttl = "",
+			dscr = "";
+		switch (d) {
+			case "Movies":
+				ttl = "Movie Sales";
+				dscr = "Top 100 Highest Grossing Movies Grouped By Genre";
+				break;
+			case "Kickstarter":
+				ttl = "Kickstarter Pledges";
+				dscr = "Top 100 Most Pledged Kickstarter Campaigns Grouped By Category";
+				break;
+			case "Video Game Sales Data Top 100":
+				ttl = "Video Game Sales";
+				dscr = "Top 100 Most Sold Video Games Grouped by Platform";
+				break;
+			default:
+				break;
+		}
+		const title = sv
+			.append("text")
+			.attr("id", "title")
+			.text(ttl);
+		const description = sv
+			.append("text")
+			.attr("id", "description")
+			.html(dscr);
+		//Centering title and description in top margin
+		title
+			.attr(
+				"x",
+				mg.l + dia[0] / 2 - title.node().getBoundingClientRect().width / 2
+			)
+			.attr("y", -(title.node().getBoundingClientRect().height / 2 + 20));
+		description
+			.attr(
+				"x",
+				mg.l + dia[0] / 2 - description.node().getBoundingClientRect().width / 2
+			)
+			.attr(
+				"y",
+				-(
+					-title.node().getBoundingClientRect().height +
+					description.node().getBoundingClientRect().height +
+					20
+				)
+			);
+	};
+
+	const generateLegend = (lgn, clr, lst) => {
+		let group = lgn
+			.selectAll("g")
+			.data(lst)
+			.enter()
+			.append("rect")
+			.attr("height", lgnd.rec)
+			.attr("width", lgnd.rec)
+			.attr("class", "legend-item")
+			.attr("fill", (d, i) => clr[i])
+			.attr("stroke", "black")
+			.attr("transform", (d, i) => {
+				if (i < 10) {
+					return `translate(${lgnd.mgl + (lgnd.mgl + lgnd.w) * i},${lgnd.mgt})`;
+				} else {
+					return `translate(${lgnd.mgl +
+						(lgnd.mgl + lgnd.w) * (i - 10)},${lgnd.mgt * 2 + lgnd.rec})`;
+				}
+			});
+
+		lgn
+			.selectAll("g")
+			.data(lst)
+			.enter()
+			.append("text")
+			.text((d) => d)
+			//			.style("fill", (d, i) => clr[i])
+			.attr("transform", (d, i) => {
+				if (i < 10) {
+					return `translate(${lgnd.mgl +
+						lgnd.rec +
+						(lgnd.mgl + lgnd.w) * i +
+						3},${lgnd.mgt + lgnd.rec - 3})`;
+				} else {
+					return `translate(${lgnd.mgl +
+						lgnd.rec +
+						(lgnd.mgl + lgnd.w) * (i - 10) +
+						3},${lgnd.mgt * 2 + lgnd.rec * 2 - 3})`;
+				}
+			});
 	};
 
 	d3.json(URL).then((data) => {
 		const layout = processData(data);
 		let dataArray = layout.descendants();
-		let colorDomain = generateColorScale(dataArray);
-		console.log(colorDomain);
-		let color = d3.scaleOrdinal(d3.schemeSpectral[11]).domain(colorDomain);
-		//.range(d3.schemeSpectral);
-		console.log(color("Narrative Film"));
-		drawDiagram(dataArray, sel, tooltipDiv, color);
+		let colorDomain = generateCategoryList(dataArray);
+		drawDiagram(dataArray, colorDomain, sel, tooltipDiv, color);
+		generateTitleDescription(sel, data.name);
+		generateLegend(legend, color, colorDomain);
 	});
-
-	// 	d3.json(
-	// 		"https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"
-	// 	).then((geo) => {
-	// 		console.log(data);
-	// 		console.log(geo);
-	// 		//Margins
-	// 		const mg = { t: 70, b: 90, r: 30, l: 70 };
-	// 		//Diag
-	// 		const d = {
-	// 			h: 600,
-	// 			w: 1000
-	// 		};
-	// 		//SVG
-	// 		const s = {
-	// 			h: mg.t + d.h + mg.b,
-	// 			w: mg.l + d.w + mg.r,
-	// 			i: "svg-canvas"
-	// 		};
-	// 		//COLORBREWER
-	// 		const clr = [
-	// 			"#ffffd9",
-	// 			"#edf8b1",
-	// 			"#c7e9b4",
-	// 			"#7fcdbb",
-	// 			"#41b6c4",
-	// 			"#1d91c0",
-	// 			"#225ea8",
-	// 			"#253494",
-	// 			"#081d58"
-	// 		];
-	// 		//Legend rect
-	// 		const lg = { h: 15, w: 50 };
-	// 		/*----------Color scale------------*/
-	// 		const color = d3
-	// 			.scaleQuantize()
-	// 			.domain([
-	// 				d3.min(data, (d) => d.bachelorsOrHigher),
-	// 				d3.max(data, (d) => d.bachelorsOrHigher)
-	// 			])
-	// 			.range(clr);
-	// 		/*----------Tooltip------------*/
-	// 		const tooltipParse = (id) => {
-	// 			let data = findData(id);
-	// 			return `${data.area_name}, ${data.state}: ${data.bachelorsOrHigher}%`;
-	// 		};
-	// 		let tooltipDiv = d3
-	// 			.select("body")
-	// 			.append("div")
-	// 			.style("opacity", 0)
-	// 			.attr("id", "tooltip")
-	// 			.attr("class", "mytooltip")
-	// 			.attr("data-education", 0);
-	// 		/*----------Map------------*/
-	// 		const findData = (id) => {
-	// 			let tmp = {};
-	// 			data.forEach((obj) => {
-	// 				if (obj.fips === id) tmp = { ...obj };
-	// 			});
-	// 			return tmp;
-	// 		};
-	// 		let path = d3.geoPath();
-	// 		const svg = d3
-	// 			.select("main")
-	// 			.append("svg")
-	// 			.attr("height", s.h)
-	// 			.attr("width", s.w);
-	// 		const map = svg
-	// 			.append("g")
-	// 			.attr("transform", `translate(${mg.l},${mg.t})`);
-	// 		map
-	// 			.selectAll("path")
-	// 			.data(topojson.feature(geo, geo.objects.counties).featuroot)
-	// 			.enter()
-	// 			.append("path")
-	// 			.attr("d", path)
-	// 			.attr("class", "county")
-	// 			.attr("fill", (d) => color(findData(d.id).bachelorsOrHigher))
-	// 			.style("stroke", "black")
-	// 			.style("stroke-width", "0.2")
-	// 			.attr("data-fips", (d) => findData(d.id).fips)
-	// 			.attr("data-education", (d) => findData(d.id).bachelorsOrHigher)
-	// 			.on("mouseover", (d) => {
-	// 				let id = d.id;
-	// 				tooltipDiv
-	// 					.html(tooltipParse(id))
-	// 					.attr("data-education", findData(id).bachelorsOrHigher)
-	// 					.style("left", d3.event.pageX + 10 + "px")
-	// 					.style("top", d3.event.pageY - 20 + "px");
-	// 				tooltipDiv
-	// 					.transition()
-	// 					.duration(100)
-	// 					.style("opacity", 0.9);
-	// 			})
-	// 			.on("mouseout", () => {
-	// 				tooltipDiv
-	// 					.transition()
-	// 					.duration(300)
-	// 					.style("opacity", 0);
-	// 			});
-	// 		map
-	// 			.append("path")
-	// 			.datum(topojson.mesh(geo, geo.objects.states, (a, b) => a !== b))
-	// 			.attr("class", "states")
-	// 			.attr("d", path);
-	// 		/*----------Legend------------*/
-	// 		const legend = svg.append("g").attr("id", "legend");
-	// 		legend
-	// 			.selectAll("rect")
-	// 			.data(clr)
-	// 			.enter()
-	// 			.append("rect")
-	// 			.attr("x", (d, i) => i * lg.w)
-	// 			.attr("height", lg.h)
-	// 			.attr("width", lg.w)
-	// 			.attr("fill", (d) => d)
-	// 			.attr("stroke", "black")
-	// 			.style("stroke-width", "0.2");
-	// 		//Legend scale
-	// 		const lgSc = d3
-	// 			.scaleLinear()
-	// 			.domain(color.domain())
-	// 			.range([0, lg.w * clr.length]);
-	// 		const tickVals = color.range().reduce((acc, d, i) => {
-	// 			let tmp = color.invertExtent(d);
-	// 			acc.push(tmp[0]);
-	// 			if (i === color.range().length - 1) acc.push(tmp[1]);
-	// 			return acc;
-	// 		}, []);
-	// 		//Legend axis
-	// 		const lgAxis = d3
-	// 			.axisBottom(lgSc)
-	// 			.tickFormat(d3.format(".1f"))
-	// 			.tickValues(tickVals)
-	// 			.tickFormat((n) => n.toFixed(1) + "%");
-	// 		//.tickFormat((n) => Math.round((n + 0.00001) * 100) / 100 + "%");
-	// 		legend
-	// 			.append("g")
-	// 			.call(lgAxis)
-	// 			.attr("id", "lg-axis")
-	// 			.attr("transform", `translate(0,${lg.h})`);
-	// 		//Positioning legend
-	// 		legend.attr(
-	// 			"transform",
-	// 			`translate(${mg.l +
-	// 				(d.w - legend.node().getBoundingClientRect().width) / 2},${mg.t})`
-	// 		);
-	// 		/*----------Title + Description------------*/
-	// 		const title = svg
-	// 			.append("text")
-	// 			.attr("id", "title")
-	// 			.text("United States Educational Attainment");
-	// 		const description = svg
-	// 			.append("text")
-	// 			.attr("id", "description")
-	// 			.html(
-	// 				"Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)"
-	// 			);
-	// 		//Centering title and description in top margin
-	// 		title
-	// 			.attr(
-	// 				"x",
-	// 				mg.l + d.w / 2 - title.node().getBoundingClientRect().width / 2
-	// 			)
-	// 			.attr("y", title.node().getBoundingClientRect().height / 2 + 20);
-	// 		description
-	// 			.attr(
-	// 				"x",
-	// 				mg.l + d.w / 2 - description.node().getBoundingClientRect().width / 2
-	// 			)
-	// 			.attr(
-	// 				"y",
-	// 				title.node().getBoundingClientRect().height +
-	// 					description.node().getBoundingClientRect().height +
-	// 					10
-	// 			);
-	// 	});
-	// });
 });
